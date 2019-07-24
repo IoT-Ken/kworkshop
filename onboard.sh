50151da603bcdffeebb77afd0545dc4631c14464
IoT-Ken <48492944+IoT-Ken@users.noreply.github.com>
2019-07-24 13:24:28 -0400

    Add files via upload

--- onboard.sh
+++ onboard.sh
@@ -10,6 +10,11 @@ cd $dirname
 
 echo "This is the script: execute phase" >> /tmp/campaign.log
 
+# Set Pulse Agent Variables
+AGENTBINPATH="/opt/vmware/iotc-agent/bin/"
+AGENTDATAPATH="/opt/vmware/iotc-agent/data/data/"
+GATEWAYID=$(cat -v ${AGENTDATAPATH}deviceIds.data | awk -F '^' '{print $2}' | awk -F '@' '{print $2}')
+
 ################################################################################
 ## Run Ansible Playbook
 ################################################################################
@@ -18,4 +23,5 @@ if [ $? -eq 0 ]; then
     echo "Ansible Installed error" >> /tmp/campaign.log
 else
     echo "Ansible command ran without error" >> /tmp/campaign.log 
+    sudo ${AGENTBINPATH}DefaultClient send-properties --device-id=$GATEWAYID --key=DesiredState --value="Onboarded"
 fi
